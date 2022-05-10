81d8a9eaef3464308ed5a3aceb503af69c389aab
João Pedro Zang Gomes <joaozanggomes@gmail.com>
2022-05-09 22:20:57 -0300

    Exercício da aula de variáveis, onde foram realizadas diversas escritas de código

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,48 @@
+/*
+Questão 1: Será impresso 10 e depois 10 , 5
+Questão 2: Será impresso 10 , 10 , 10
+Questão 3: let = p (colocaria como let = horasDia) let = t (colocaria como let = valorDia)
+*/
+
+// 1.
+
+//let idade;
+//let nome;
+//console.log (typeof nome, typeof idade);
+// Foi impresso undefined por causa que estas variaveis não tiveram um valor atribuido
+
+//nome = prompt ("Qual é o seu nome?");
+//idade = prompt ("Qual é sua idade?");
+//console.log (typeof nome, typeof idade);
+//console.log ("Olá", nome, "você tem", idade, "anos");
+
+/* 2.
+
+let doce = prompt ("Você gosta de doces?");
+let dançar = prompt ("Você gosta de dançar?");
+let cachorros = prompt ("Você gosta de cachorros?");
+console.log ("Você gosta de doces? - ", doce);
+console.log ("Você gosta de dançar? - ", dançar);
+console.log ("Você gosta de cachorros? -", cachorros);
+*/
+
+/* 3.
+
+let a = 10;
+let b = 25;
+c = a;
+a = b; 
+b = c; 
+console.log ("O novo valor de a é", a);
+console.log ("O novo valor de b é", b);
+*/
+
+/* Desafio:
+
+let numero1 = Number (prompt ("Seu primeiro número"));
+let numero2 = Number (prompt ("Seu segundo número"));
+s = numero1 + numero2;
+m = numero1 * numero2;
+console.log ("O primero número somado ao segundo número resulta em:", s);
+console.log ("O primeiro número multiplicado pelo segundo número resulta em:", m);
+*/
