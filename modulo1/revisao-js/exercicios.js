7a37b5736c82238927aad8f55c1a1f657041fb1f
João Pedro Zang Gomes <joaozanggomes@gmail.com>
2022-05-23 18:42:09 -0300

    Até o exercicio 8

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -25,26 +25,66 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+    let arrayPares = []
+  for (let i = 0; i < array.length; i++){
+      if (array[i] % 2 === 0){
+        arrayPares.push(array[i])
+      }
+  } return arrayPares
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    let arrayElevado = []
+    for (let i = 0; i < array.length; i++){
+        if (array[i] % 2 ===0){
+            arrayElevado.push(array[i]*array[i])
+        }
+    }return arrayElevado
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+    let arrayMaiorNum = []
+    for (let i = 0; i < array.length; i++){
+        if (array[i] > arrayMaiorNum){
+            arrayMaiorNum.pop()
+            arrayMaiorNum.push(array[i])
+        }
+    } 
+    let arrayMaiorNum2 = arrayMaiorNum[0]
+    return arrayMaiorNum2
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-
+    let maiorNum = 0
+    let maiorDivisivelPeloMen = 0
+    let diferenc = 0
+    let objeto1 = {maiorNumero: 0,}
+    let objeto2 = {maiorNumero: 0,} 
+    if (num1 > num2){
+        maiorNum = num1
+        maiorDivisivelPeloMen = num1 % num2 === 0
+        diferenc = num1 - num2
+        objeto1.maiorNumero = maiorNum
+        objeto1.maiorDivisivelPorMenor = maiorDivisivelPeloMen,
+        objeto1.diferenca = diferenc 
+           return objeto1
+        }  else{
+        maiorNum = num2
+        maiorDivisivelPeloMen = num2 % num1 === 0
+        diferenc = num2 - num1
+        objeto2.maiorNumero = maiorNum,
+        objeto2.maiorDivisivelPorMenor= maiorDivisivelPeloMen,
+        objeto2.diferenca = diferenc 
+            return objeto2
+}
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
+    
    
 }
 
