ddcdbcd357ee163ab6aa6e2053185772dab4034c
João Pedro Zang Gomes <joaozanggomes@gmail.com>
2022-06-14 00:56:53 -0300

    Finalizado

--- modulo2/rend-condicional-form/rend-condicional-form/src/components/Etapa2.js
+++ modulo2/rend-condicional-form/rend-condicional-form/src/components/Etapa2.js
@@ -1,11 +1,36 @@
-class Etapa2 extends React.Component {
+import React from "react";
+import PerguntaAberta from "./PerguntaAberta";
+import styled from "styled-components";
+
+const Container1 = styled.div `
+    margin: 0 auto;
+    padding-bottom: 1.5% ;
+    width: 40%;
+    height: 40%; 
+    border: 1px solid;
+    border-radius: 60px 60px;
+    background-color: lightskyblue;
+    box-shadow: 5px 5px black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+
+
+
+export class Etapa2 extends React.Component {
     render (){
       return (
-        <div>
-        
-        </div>
+        <Container1>
+            <h2>ETAPA 2 - INFORMAÇÕES DO ENSINO SUPERIOR</h2>
+            <PerguntaAberta pergunta={"5. Qual curso?"} />
+            <PerguntaAberta pergunta={"6. Qual a unidade de ensino?"} />
+            {/* <button>Próxima etapa</button> */}
+        </Container1>
       );
     }
   }
-  export default Etapa2;
+  
   
