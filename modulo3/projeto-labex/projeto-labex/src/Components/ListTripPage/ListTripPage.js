2fedeb37e089c9c74f8ddae09e849203538157d8
João Pedro Zang Gomes <joaozanggomes@gmail.com>
2022-07-14 06:44:21 -0300

    Parte 1 da lista de viagens

--- modulo3/projeto-labex/projeto-labex/src/Components/ListTripPage/ListTripPage.js
+++ modulo3/projeto-labex/projeto-labex/src/Components/ListTripPage/ListTripPage.js
@@ -1,11 +1,29 @@
 import React from "react";
+import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import { Container } from "./styled";
+import { Container, ContainerTop, ContainerBottom, Title, TitleStyleP, ContainerTrips } from "./styled";
+import { useState, useEffect } from "react";
 
 
 export default function ListTripPage (props) {
+
+    const [trips, setTrips] = useState()
+
+    useEffect(() => {
+      getTrips()
+    }, []) 
+
+    const getTrips = async() => {
+      try{
+        const response = await axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labeX/joao-gomes-ailton/trips")
+        console.log(response.data.trips)
+      } catch(error){
+
+      }
+    }
+
 
     const navigate = useNavigate()
 
@@ -16,7 +34,19 @@ export default function ListTripPage (props) {
       <div>
         <Header notLoged={props.notLoged} setNotLogedToReverse={props.setNotLogedToReverse}/>
         <Container>
-          
+
+            <ContainerTop>
+                <Title>
+                    <TitleStyleP>Viagens Disponíveis</TitleStyleP>
+                </Title>
+            </ContainerTop>
+                
+            <ContainerBottom>
+                <ContainerTrips>
+
+                </ContainerTrips>
+            </ContainerBottom>
+
         </Container>
         <Footer/>
       </div>
--- modulo3/projeto-labex/projeto-labex/src/Components/ListTripPage/styled.js
+++ modulo3/projeto-labex/projeto-labex/src/Components/ListTripPage/styled.js
@@ -2,6 +2,43 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 width: 100%;
-height: 70vh;
+height: 100vh;
 background-image: radial-gradient(circle, #f8cecc, #f6ced6, #f1d0df, #ead2e7, #e1d5ed);
+display: flex;
+flex-direction: column;
+`
+export const ContainerTop = styled.div`
+/* border-bottom: 1px solid black; */
+width: 100%;
+height: 20%;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+export const Title = styled.div`
+width: 25%;
+height: 40%;
+background-color: white;
+border: 1px solid black;
+border-radius: 40px;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+export const TitleStyleP = styled.p `
+font-size: 1.5rem;
+`
+export const ContainerBottom = styled.div`
+/* border: 1px solid black; */
+width: 100%;
+height: 70%;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+export const ContainerTrips= styled.div `
+width: 70%;
+height: 80%;
+background-color: white;
+border: 1px solid black;
 `
