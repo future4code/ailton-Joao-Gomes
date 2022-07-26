8f28459399364931d55b3fc1055d6b9d7a9af28e
João Pedro Zang Gomes <joaozanggomes@gmail.com>
2022-07-26 18:42:11 -0300

    lógica de logout

--- modulo3/projeto-labeddit/labeddit/src/components/Header/Header.jsx
+++ modulo3/projeto-labeddit/labeddit/src/components/Header/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Container, DivButton, LogoStyle } from './style'
 import LogoLabeddit from "../../assets/logolabeddit.png";
+import { GoTo } from '../../functions/GoTo';
+import { useNavigate } from "react-router";
 
 export const Header = () => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate()
+
+  const logout = () => {
+    window.localStorage.setItem("token", "");
+    window.alert("Você deslogou.");
+    GoTo(navigate,"/");
+  };
+
   return (
     <Container>
-        <LogoStyle src={LogoLabeddit} />
-        <DivButton>Entrar</DivButton>
+      <LogoStyle src={LogoLabeddit} />
+      {token ? <DivButton onClick={logout}>Logout</DivButton> : <DivButton onClick={()=> GoTo(navigate,"/")}>Entrar</DivButton>}
+      
     </Container>
-  )
+  );
 }
